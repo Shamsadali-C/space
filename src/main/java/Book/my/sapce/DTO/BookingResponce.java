16b3e7ecea738cf5f91d86668e73c6982f191f0f
Shamsadali.C <Shamsadalic@gmail.com>
2026-07-10 10:26:52 +0530

    1

---
+++ src/main/java/Book/my/sapce/DTO/BookingResponce.java
@@ -0,0 +1,16 @@
+//package Book.my.sapce.DTO;
+//
+//import Book.my.sapce.Model.User;
+//import Book.my.sapce.Model.Venue;
+//import lombok.Data;
+//
+//import java.time.LocalDate;
+//@Data
+//public class BookingResponce {
+//   private Long id;
+//   private User user;
+//   private Venue venue;
+//   private LocalDate date;
+//   private String status;
+//
+//}
