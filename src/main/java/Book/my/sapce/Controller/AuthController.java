ca12ece2e139ae36491ea943b6495dce4a4982fd
Shamsadali.C <Shamsadalic@gmail.com>
2026-08-03 17:27:06 +0530

    Security completed..

---
+++ src/main/java/Book/my/sapce/Controller/AuthController.java
@@ -0,0 +1,33 @@
+package Book.my.sapce.Controller;
+
+import Book.my.sapce.Model.User;
+import Book.my.sapce.Repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+public class AuthController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    private BCryptPasswordEncoder encoder= new BCryptPasswordEncoder(12);
+
+
+    @PostMapping
+    public ResponseEntity<String> register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+}
---
+++ src/main/java/Book/my/sapce/Security/SecurityConfig.java
@@ -0,0 +1,83 @@
+package Book.my.sapce.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+//    @Autowired
+//    public UserDetailsService userDetailsService;
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http.csrf(csrf -> csrf.disable())
+               .authorizeHttpRequests(auth -> auth
+                       .requestMatchers("/register").permitAll()
+                       .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                       .requestMatchers("/user/**").hasAuthority("USER")
+                       .requestMatchers("/venue/**").hasAuthority("OWNER")
+                       .requestMatchers("/booking").hasAnyAuthority("USER","OWNER")
+                       .anyRequest().authenticated()
+               )
+
+               .formLogin(Customizer.withDefaults())
+               .logout(log ->log
+                       .logoutUrl("/logout")
+                       .logoutSuccessUrl("/login?logout")
+                       .permitAll())
+               .httpBasic(Customizer.withDefaults());
+
+
+       return http.build();
+
+
+//           .sessionManagement(session->
+//               session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+   }
+
+
+   @Bean
+   public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService){
+       DaoAuthenticationProvider provider=new DaoAuthenticationProvider(userDetailsService);
+       provider.setPasswordEncoder(passwordEncoder());
+//       provider.setUserDetailsService(userDetailsService);
+               return provider;
+   }
+
+//   @Bean  //just for first user adding
+//    public UserDetailsService userDetailsService(){
+//       UserDetails user= User.builder()
+//               .username("user")
+//               .password(passwordEncoder().encode("user123"))
+//               .roles("USER")
+//               .build();
+//
+//       return new InMemoryUserDetailsManager(user,admin);
+//    }
+
+
+}
