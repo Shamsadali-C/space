ec9e5b5eaa0969eebc4b4713d91efe85392570ea
Shamsadali.C <Shamsadalic@gmail.com>
2026-08-03 17:27:21 +0530

    Security completed..

---
+++ src/main/java/Book/my/sapce/Service/MyUserDetailsService.java
@@ -0,0 +1,46 @@
+package Book.my.sapce.Service;
+
+import Book.my.sapce.Model.User;
+import Book.my.sapce.Model.UserPrinciple;
+import Book.my.sapce.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()->new UsernameNotFoundException("User not found"));
+
+
+//                if(user==null) {
+//                    System.out.println("user not found");
+//                   throw new UsernameNotFoundException("User not found");
+//                }
+
+//                return  new UserPrinciple(user);
+
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username((user.getUsername()))
+                .password((user.getPassword()))
+                .authorities(user.getRole().name())
+                .build();
+
+
+
+    }
+}
+
